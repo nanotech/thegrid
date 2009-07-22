daebca7b67a12be633292295151942f7472714bf
NanoTech <nanotech008@gmail.com>
2009-07-21 22:52:41 -0600

    Set Program traits with meta-programming.

--- lib/program.rb
+++ lib/program.rb
@@ -29,6 +29,40 @@ class Program
 
 	private
 
-	def self.length n; @length = n end
-	def self.moves n; @moves = n end
+	# Metaprogramming stuff originally from:
+	# why's (poignant) guide to ruby, chapter 6
+
+	# Get a metaclass for this class
+	def self.metaclass; class << self; self; end; end
+
+	# Advanced metaprogramming code for nice, clean traits
+	def self.traits *arr
+		return @traits if arr.empty?
+
+		# 1. Set up accessors for each variable
+		attr_accessor *arr
+
+		# 2. Add a new class method to for each trait.
+		arr.each do |a|
+			metaclass.instance_eval do
+				define_method(a) do |val|
+					@traits ||= {}
+					@traits[a] = val
+				end
+			end
+		end
+
+		# 3. For each program, the `initialize' method
+		#    should use the default number for each trait.
+		class_eval do
+			define_method :initialize do
+				self.class.traits.each do |k,v|
+					instance_variable_set("@#{k}", v)
+				end
+			end
+		end
+	end
+
+	# Program attributes are read-only
+	traits :length, :moves, :icon
 end
